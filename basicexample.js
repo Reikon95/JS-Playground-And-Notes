a186fcb4bdb6ae06b2f570b4f13142540e40a054
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-02-12 13:10:04 +0000

    Create basicexample.js

---
+++ basicexample.js
@@ -0,0 +1,8 @@
+function sayThanks(name){
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+
+sayThanks('Cameron');
+
+//function is called saythanks with an argument of name - changing the argument on line 5 to Cameron generates a string of 
+Thank you for your purchase Cameron! We appreciate your business.
