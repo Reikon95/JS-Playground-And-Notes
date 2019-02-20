a36a579a1afa8ae20b1b9a24def5d5e83b603a75
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-02-20 15:44:47 +0000

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,13 @@
+//example of an object. Objects are similar to arrays, except that instead of using indexes to access and modify their data, 
+you access the data in objects through what are called properties.
+
+
+
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+//name legs tails and friends are all PROPERTIES
