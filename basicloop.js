021068339da4666a788c85c8370b7ea6a6d4f83c
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-03-22 15:24:52 +0000

    Update basicloop.js

--- basicloop.js
+++ basicloop.js
@@ -6,3 +6,17 @@ while(i > 100){
   i++;
   }
 
+let bobsFollowers = ['Fred', 'Adam', 'Josh', 'David'];
+let tinasFollowers = ['Brenda', 'Fred', 'Josh'];
+
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++){
+  for (let j = 0; j < tinasFollowers.length; j++){
+    if (bobsFollowers[i] === tinasFollowers[j]){
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+}
+
+console.log(mutualFollowers);
